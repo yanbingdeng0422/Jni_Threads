d84a704e6fdc0a17b2b3563ef26ab68cca093eb1
yanbing.deng0422 <yanbing.deng0422@gmail.com>
2020-04-24 07:16:33 +0800

    [原生线程] Native调用java线程

--- jni/jni_threads.c
+++ jni/jni_threads.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <utils/Log.h>
 
+#include <pthread.h>
+
 #include <stdio.h>
 
 #define LOG_TAG "threads-jni"
@@ -19,8 +21,57 @@ static jstring com_yanbing_jni_threads_MainActivity_stringFromJNI(JNIEnv* env, j
     return (*env)->NewStringUTF(env,"Hello from C++");
 }
 
+static jmethodID gOnNativeMessage = NULL;
+
+static JavaVM* gVm = NULL;
+static jobject gObj =NULL;
+static void com_tyanbing_jni_threads_MainActivity_nativeInit(JNIEnv* env, jobject obj){
+    if(NULL == gOnNativeMessage){
+        jclass class = (*env) -> GetObjectClass(env,obj);
+
+        gOnNativeMessage = (*env)->GetMethodID(env,class,"onNativeMessage","(Ljava/lang/String;)V");
+
+        if(NULL == gOnNativeMessage){
+            jclass exceptionClass = (*env) ->FindClass(env,"java/lang/RuntimeExcetion");
+            (*env) -> ThrowNew(env,exceptionClass,"Unable to find method");
+        }
+    }
+}
+static void com_yanbing_jni_threads_MainActivity_nativeFree(JNIEnv* env, jobject obj){
+
+}
+
+static void com_yanbing_jni_threads_MainActivity_nativeWorker(JNIEnv* env, jobject obj,jint id ,jint iterations){
+    for(jint i=0;i<iterations;i++){
+        char message[26];
+        sprintf(message, "Worker %d:Iteration %d",id,i);
+        jstring messageString = (*env)->NewStringUTF(env,message);
+        (*env) -> CallVoidMethod(env,obj, gOnNativeMessage,messageString);
+        if(NULL != (*env) ->ExceptionOccurred(env)){
+            break;
+        }
+        sleep(1);
+    }
+}
+
+struct NativeWorkerArgs{
+    jint id;
+    jint iterations;
+}
+
+
+
+static void com_yanbing_jni_threads_MainActivity_posixThreads(JNIEnv* env, jobject obj,jint id ,jint iterations){
+
+
+}
+
 static JNINativeMethod methods[] = {
   {"stringFromJNI", "()Ljava/lang/String;", (void*)com_yanbing_jni_threads_MainActivity_stringFromJNI },
+  {"nativeInit","()V",(void*)com_tyanbing_jni_threads_MainActivity_nativeInit},
+  {"nativeFree","()V",(void*)com_yanbing_jni_threads_MainActivity_nativeFree},
+  {"nativeWorker","(II)V",(void*)com_yanbing_jni_threads_MainActivity_nativeWorker},
+  {"posixThreads","(II)V",(void*)com_yanbing_jni_threads_MainActivity_posixThreads},
 };
 
 /*
